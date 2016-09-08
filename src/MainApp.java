97f8b85cd573433505118f8683133c66876309e0
Kevin Baxter <kbax6342@gmail.com>
2016-09-08 11:23:17 -0400

    Initial Commit

---
+++ src/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ *This is a demo app for deciding what to wear.
+ * 
+ */
+
+/**
+ * @author kbaxter
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
